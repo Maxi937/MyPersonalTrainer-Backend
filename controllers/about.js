4de03bf30d3a1cafc8ab4585d8a8070d5e92a6eb
Matthew <mhornby123@gmail.com>
2022-11-17 22:47:09 +0000

    aout page check for client

--- controllers/about.js
+++ controllers/about.js
@@ -6,8 +6,15 @@ const accounts = require("./accounts.js")
 const about = {
   index(request, response) {
     logger.info("Rendering About");
+    console.log(request.session)
+
+
+    const client = false || request.session.client
+
+    console.log(client)
 
     const viewData = {
+      client,
       title: "About",
     };
     response.render("about", viewData);
