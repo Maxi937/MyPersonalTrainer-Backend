b516c518bb93c3ce12690a9fa6494436ec6601f1
mhornby123@gmail.com <mhornby123@gmail.com>
2022-12-11 16:31:07 +0000

    test4

--- controllers/deedlockerPi.js
+++ controllers/deedlockerPi.js
@@ -11,7 +11,7 @@ const deedlockerPi = {
       message: "Hello here is some data",
       data: "49 is the meaning of life"
     };
-    response.status(200).send(responseData);
+    response.status(200).json(responseData);
   },
 };
 
