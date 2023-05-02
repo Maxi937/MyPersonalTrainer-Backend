ec8756057a88268be656a9f066eb05a66b21529a
Matthew Hornby <70072337+Maxi937@users.noreply.github.com>
2023-05-02 19:30:13 +0100

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -5,11 +5,11 @@ TCP_CONN=$(netstat -an | wc -l)
 TCP_CONN_PORT_3000=$(netstat -an | grep 3000 | wc -l)
 IO_WAIT=$(iostat | awk 'NR==4 {print $5}')
 
-aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY
+aws cloudwatch put-metric-data --metric-name beerconomy-memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY
 
-aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
+aws cloudwatch put-metric-data --metric-name beerconomy-Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
 
-aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_3000
+aws cloudwatch put-metric-data --metric-name beerconomy-TCP_connection_on_port_3000 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_3000
 
-aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT
+aws cloudwatch put-metric-data --metric-name beerconomy-IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT
 
