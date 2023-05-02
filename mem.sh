5a5f062ff34dc217f4b9c771d7e787e27f544012
Matthew Hornby <70072337+Maxi937@users.noreply.github.com>
2023-05-02 17:10:56 +0100

    Create mem.sh

---
+++ mem.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+USEDMEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
+TCP_CONN=$(netstat -an | wc -l)
+TCP_CONN_PORT_3000=$(netstat -an | grep 3000 | wc -l)
+IO_WAIT=$(iostat | awk 'NR==4 {print $5}')
+
+aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$I
+NSTANCE_ID --namespace "Custom" --value $USEDMEMORY
+
+aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance
+=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
+
+aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimension
+s Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_3000
+
+aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTAN
+CE_ID --namespace "Custom" --value $IO_WAIT
+
+chmod +x mem.sh
+
+env EDITOR=nano crontab -e
+*/1 * * * * /home/ec2-user/mem.sh
