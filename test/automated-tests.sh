e0ebbedfac2789c950260e124481a45d85e9155b
Matthew <mhornby123@gmail.com>
2023-08-20 17:34:20 +0100

    Windows notifcation on test completion

--- test/automated-tests.sh
+++ test/automated-tests.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-output=$(npm run testall)
 
-echo "$output"
+# This script is called on git pre-commit via wsl
 
 # source my functions for notify-send function
 source ~/myFunctions
 
-# WSL-Notify Function
+# Run tests
+numberOfTests=$(npm run testalldry | grep -o -E '[[:digit:]]?[[:digit:]] passing' | grep -o -E '[[:digit:]]?[[:digit:]]')
+testOutput=$(npm run testall)
+passingTests=$(echo "$testOutput" | grep -c -E '✔')
+echo "$testOutput"
+
+# notify-send - Send Windows Notification when complete
 category="Personal Trainer Automated Tests"
 icon="C:\Users\Matthew\Pictures\wsl-notify-icons\ubuntu.webp"
+message="$passingTests/$numberOfTests passing"
 
-notify-send -c "$category" -i "$icon" "$output";
+notify-send -c "$category" -i "$icon" "$message";
