898969af9aa2b226a0a05ec80d8b538a590526d3
Matthew <mhornby123@gmail.com>
2023-08-26 18:05:19 +0100

    test

--- test/automated-tests.sh
+++ test/automated-tests.sh
@@ -6,6 +6,14 @@
 # source my functions for notify-send function
 source ~/myFunctions
 
+# Send Windows Notification when complete
+category="Personal Trainer Automated Tests"
+icon="C:\Users\Matthew\Pictures\wsl-notify-icons\ubuntu.webp"
+message="Running Automated Tests"
+
+# Send Toast
+notify-send -c "$category" -i "$icon" "$message";
+
 # Run tests
 # dry run to get total number of tests
 numberOfTests=$(npm run testalldry | grep -o -E '[[:digit:]]?[[:digit:]] passing' | grep -o -E '[[:digit:]]?[[:digit:]]')
