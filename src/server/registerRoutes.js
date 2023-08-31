ab2f8d73fb4e117fa6b420a2f0ac8e40e12a2b1e
Matthew <mhornby123@gmail.com>
2023-08-31 18:10:35 +0100

    + fix register routes

--- src/server/registerRoutes.js
+++ src/server/registerRoutes.js
@@ -1,11 +1,7 @@
 import fs, { fstat } from "fs";
 import path from "path";
+import url from "url";
 import _ from "lodash";
-
-// Each Feature is imported if its in the "features" folder.
-// The default export of each Feature is an array of Objects with one key.
-// The route is the value of this object key.
-// Each route is parsed for the Method, Path and the rest of the properties are put into an "options" object for Hapi's server.route() function
 
 /**
  * Builds an array of file paths in a directory and any sub-directories.
@@ -28,7 +24,8 @@ function traceFilePaths(directoryPath) {
 }
 
 /**
- * Parses each given filepath for a default export - which will be an object containing each route & rearranges the properties to be Hapi compatible
+ * Parses each given filepath for a default export - which will be an object containing each route & rearranges the properties to be Hapi compatible.
+ * Each File Path is converted to a URL filepath before Parse to be compatible with Git / Node.
  * @param {Array<string>} importList - A list of filepaths to parse.
  * @returns {Array<object>} A list of route objects compatible with Hapi.
  */
@@ -37,7 +34,7 @@ async function getRoutes(importList) {
 
   await Promise.all(
     importList.map(async (im) => {
-      const i = await import(im);
+      const i = await import(url.pathToFileURL(im).href);
       if (i.default) {
         Object.keys(i.default).forEach((key) => {
           const result = {
@@ -53,13 +50,21 @@ async function getRoutes(importList) {
   return routesList;
 }
 
-export async function registerRoutes() {
-  const featuresPath = "./src/features";
+/**
+ * Each Feature is imported if its in the "features" folder.
+ * The default export of each Feature is an array of Objects with one key.
+ * The route is the value of this object key.
+ * Each route is parsed for the "Method", "Path" with the rest of the properties being put into an "Options" object for Hapi's server.route() function.
+ * @param {string} [featuresPath="./src/features"] - The Path the features folder. defaults to: "./src/features".
+ * @returns {Array<Object>} Sum of a and b
+ */
+export async function registerRoutes(featuresPath = "./src/features") {
   const features = fs.readdirSync(featuresPath);
   const importList = [];
 
   features.forEach((feature) => {
-    importList.push(...traceFilePaths(path.resolve(featuresPath, feature)));
+    const featurePath = path.resolve(featuresPath, feature);
+    importList.push(...traceFilePaths(featurePath));
   });
 
   const featureRoutes = getRoutes(importList);
